616dfd4fc49981ad580bda84622e61029fc59706
Stewart898 <65672107+Stewart898@users.noreply.github.com>
2024-08-28 06:56:41 +0000

    feat: initiated the project

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("c0b10a030ea7e6157c3e39105bac18784b2874d7c68bd8139a947a48045c4402","dfgdfg"){} 
+    constructor()ERC20("gdfg","dfgd"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
