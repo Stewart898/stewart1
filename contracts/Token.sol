f916cd8cac6acb1064165af42a468c0df367fbfb
Stewart898 <65672107+Stewart898@users.noreply.github.com>
2024-08-28 06:44:59 +0000

    feat: initiated the project

---
+++ contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor()ERC20("c0b10a030ea7e6157c3e39105bac18784b2874d7c68bd8139a947a48045c4402","dfgdfg"){} 
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function burn100tokens() public{
+        _burn(msg.sender, 100*10**18);
+    }
+}
